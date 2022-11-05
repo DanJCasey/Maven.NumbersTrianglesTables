57e851fe72b50bcba356c077f29224095ad5ebb2
Daniel <daniel@Zipcoders-MacBook-Pro-10.local>
2022-11-05 19:13:05 -0400

    finally finished putting the code in place and all tests pass and understood

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -2,14 +2,35 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        String table = "";
+        for (int column = 1; column <= 5; column++) {
+            for(int mod = 1; mod <= 5; mod++){
+                table += String.format("%3s |",(column * mod));
+            }
+            table += "\n"
+;        }
+        return table;
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        String table = "";
+        for (int column = 1; column <= 10; column++) {
+            for(int mod = 1; mod <= 10; mod++){
+                table += String.format("%3s |",(column * mod));
+            }
+            table += "\n"
+            ;        }
+        return table;
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        String table = "";
+        for (int column = 1; column <= tableSize; column++) {
+            for(int mod = 1; mod <= tableSize; mod++){
+                table += String.format("%3s |",(column * mod));
+            }
+            table += "\n"
+            ;        }
+        return table;
     }
 }
